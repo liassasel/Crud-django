fa36432e9e39c6bba05c22f6adb60a37b321cd09
Angel <auseche80@gmail.com>
2024-09-20 11:24:25 -0400

    Second commit

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
+
+
 # Create your views here.
 
 def home(request):
@@ -15,13 +18,21 @@ def signup(request):
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
-        #register user
             try:
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created successfully')
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'User already exist'
+                })
+            
+        return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Password do not match'
+                })
 
-            except:
-                return HttpResponse('Username already exists')
-        return HttpResponse('Password do not match')
-
+def tasks(requiest):
+    return render(requiest, 'tasks.html')
