a3dc49c9b4166f9649b67112a2a459e02916f51c
Angel <auseche80@gmail.com>
2024-09-26 12:41:52 -0400

    Tailwind Installation

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'tasks',
+    'tailwind',
+    'my_app'
 ]
+
+TAILWIND_APP_NAME = 'my_app'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+LOGIN_URL = '/signin'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
